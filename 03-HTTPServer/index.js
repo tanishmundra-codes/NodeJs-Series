71a946aa67fc36a4d6b7430b79e3cade4d6dcb68
tanishmundra-codes <tanishmundra2@gmail.com>
2025-12-20 12:35:07 +0530

    undertand URL

--- 03-HTTPServer/index.js
+++ 03-HTTPServer/index.js
@@ -1,10 +1,29 @@
 const http = require("http");
 const fs = require("fs");
+const url = require('url');
 
 const server = http.createServer((req, res) => {
-    const log = `${Date.now()}\n`;
+    if(req.url === "/favicon.ico") return res.end();
+    const log = `${Date.now()}:${req.url}\n`;
+
+    const myUrl = url.parse(req.url, true);
+    console.log(myUrl);
+
     fs.appendFile("./log.txt", log, (err , data) => {
-        res.end("Hello from server");
+        switch(myUrl.pathname){
+            case "/" :
+                res.end("Home Page")
+                break;
+            case "/about" :
+                res.end("About Page")
+                break;
+            case "/search":
+                const search = myUrl.query.search_query;
+                res.end("Here is the serach result :" + search);
+                break;
+            default:
+                res.end("404 Error");
+        }
     });
 })
 
