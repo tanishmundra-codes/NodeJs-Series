95b7f61ddd154b06c21f3b30494bfe854edf1358
tanishmundra-codes <tanishmundra2@gmail.com>
2025-12-23 17:59:49 +0530

    POST api implemention using postman

--- 05-RESTAPI/index.js
+++ 05-RESTAPI/index.js
@@ -1,13 +1,17 @@
 const express = require("express");
+const fs = require("fs");
 const users = require("./USERS_DATA.json");
+const { json } = require("stream/consumers");
 
 const app = express();
 const PORT = 3000;
 
+// Middleware -> to get the POST api data in body
+app.use(express.urlencoded({extended : false}));
+
 // Client Side Rendering => /api/user
 // Server Side Rendering => /user
 // This is called Hybrid Server
-
 
 app.route("/api/users/:id")
 .get((req, res) => {
@@ -23,14 +27,17 @@ app.route("/api/users/:id")
     return res.json({status : "Pending"});
 })
 
-
 app.route("/api/users")
 .get((req,res) => {
     return res.json(users);
 })
 .post((req,res) => {
-    return res.json({status : "Pending"});
-})
+    const body = req.body;
+    users.push({...body, id: users.length + 1});
+    fs.writeFile("./USERS_DATA.json", JSON.stringify(users), (err, result) => {
+        return res.json({status : "sucess"});
+    });
+});
 
 app.listen(PORT, () => {
     console.log(`Server started at PORT: ${PORT}`);
