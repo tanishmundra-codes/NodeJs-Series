3a5e4fb1f0ab1fbb348aa758a997115d960434f0
tanishmundra-codes <tanishmundra2@gmail.com>
2026-01-05 17:38:47 +0530

    multer

--- 08-fileUploading/index.js
+++ 08-fileUploading/index.js
@@ -1,17 +1,25 @@
 const express = require("express");
 const path = require("path")
+const multer = require('multer')
 
 const app = express();
 const PORT = 3000;
+
+const upload = multer({dest : "upload/"})
 
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
 
 app.use(express.json());
+app.use(express.urlencoded({extended: false}))
 
 app.get("/", (req, res) => {
     return res.render("homepage");
 });
+
+app.post("/upload", upload.single("profileImage"), (req, res) => {
+
+})
 
 app.listen(PORT, () => {
     console.log(`Server Started at PORT : ${PORT}`);
